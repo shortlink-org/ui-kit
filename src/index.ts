930bd3ad579d1718e1aa9fb6a6ba6508eedbd863
Viktor Login <batazor111@gmail.com>
2025-03-15 20:43:48 +0700

    ui-kit: init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// theme exports
+export { theme } from './theme/theme'
+
+// Components exports
+export { ToggleDarkMode } from './ui/ToggleDarkMode/ToggleDarkMode'
+export { GithubRepository } from './card/GithubRepository/GithubRepository'
+export { Timeline } from './ui/Timeline/Timeline'
+export { default as Table } from './ui/Table/Table'
+export { default as ScrollToTopButton } from './ui/ScrollToTopButton/ScrollToTopButton'
+
+// Input
+export { default as SearchForm } from './ui/SearchForm/SearchForm'
+
+// Layout exports
+export { Header } from './page/Header/Header'
+export { Sidebar } from './page/Sidebar/Sidebar'
+export { PriceTable } from './page/PriceTable/PriceTable'
