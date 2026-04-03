a839d8e1370e3e90c467cd0b6378ef1e8365b65c
Viktor Login <batazor111@gmail.com>
2026-04-03 22:09:23 +0300

    1.8.6

--- types/missing-modules.d.ts
+++ types/missing-modules.d.ts
@@ -22,3 +22,11 @@ declare module '*.module.css' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
+
+declare module '*.css'
+
+declare module '@fontsource/*'
+
+declare module '#.storybook/preview' {
+  export { default } from '../.storybook/preview'
+}
